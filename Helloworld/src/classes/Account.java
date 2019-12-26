9f26fe184481b6e97a1e000c82358b41feda2b5e
User <User@YD01-07>
2019-12-26 14:00:18 +0900

    dd

--- Helloworld/src/classes/Account.java
+++ Helloworld/src/classes/Account.java
@@ -1,5 +1,20 @@
 package classes;
 
 public class Account {
-
+	private String ano;
+	private String owner;
+	private int balance;
+	
+	public Account(String ano, String owner, int balance) {
+		this.ano = ano;
+		this.owner = owner;
+		this.balance = balance;
+	}
+	
+	public String getAno() { return ano; }
+	public void setAno(String ano) {this.ano = ano; }
+	public String getOwner() { return owner; }
+	public void serOwner(String owner) { this.owner = owner; }
+	public int getBalance() { return balance; }
+	public void serBalance(int balance) {this.balance = balance;}
 }
